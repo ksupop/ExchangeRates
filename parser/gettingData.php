6fecebe64668e7967c2c5b6f1ce06176cfb6454b
ksupop <48125504+ksupop@users.noreply.github.com>
2024-04-28 12:12:57 +0300

    Add files via upload

--- parser/gettingData.php
+++ parser/gettingData.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include "db.php";
+include "../db.php";
 //$CurrentDate = "02/03/2002";
 $CurrentDate = date('d/m/Y');
 
