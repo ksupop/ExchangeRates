054d1827caf8722d3afedee0ecedfeb7e4052d8b
ksupop <48125504+ksupop@users.noreply.github.com>
2024-04-30 10:45:33 +0300

    Add files via upload

--- post_scripts/addValute.php
+++ post_scripts/addValute.php
@@ -2,18 +2,23 @@
 
 include "../db.php";
 
-$id = pg_escape_string($_POST['ValID']);
-$numcode = pg_escape_string($_POST['ValNumcode']);
-$charcode = pg_escape_string($_POST['ValCharcode']);
-$nominal = pg_escape_string($_POST['ValNominal']);
-$name = pg_escape_string($_POST['ValName']);
-$value = pg_escape_string($_POST['ValValue']);
-$vunitrate = pg_escape_string($_POST['ValVunitrate']);
-$valdate = pg_escape_string($_POST['ValDate']);
-
-$query = "INSERT INTO valute (id, numcode, charcode, nominal, name, value, vunitrate, valdate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
-$params = array($id, $numcode, $charcode, $nominal, $name, $value, $vunitrate, $valdate);
-
-$addResult = pg_query_params($dbconn, $query, $params) or die ('Ошибка');
+if(isset($_POST['ValID'], $_POST['ValNumcode'], $_POST['ValCharcode'], $_POST['ValNominal'], $_POST['ValName'], $_POST['ValValue'], $_POST['ValVunitrate'], $_POST['ValDate'])) {
+    $id = pg_escape_string($_POST['ValID']);
+    $numcode = pg_escape_string($_POST['ValNumcode']);
+    $charcode = pg_escape_string($_POST['ValCharcode']);
+    $nominal = pg_escape_string($_POST['ValNominal']);
+    $name = pg_escape_string($_POST['ValName']);
+    $value = pg_escape_string($_POST['ValValue']);
+    $vunitrate = pg_escape_string($_POST['ValVunitrate']);
+    $valdate = pg_escape_string($_POST['ValDate']);
+    
+    $query = "INSERT INTO valute (id, numcode, charcode, nominal, name, value, vunitrate, valdate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+    $params = array($id, $numcode, $charcode, $nominal, $name, $value, $vunitrate, $valdate);
+    
+    $addResult = pg_query_params($dbconn, $query, $params) or die ('Ошибка');
+} else {
+    die('Отсутствуют необходимые данные');
+}
 
 ?>
+
--- post_scripts/getGraphData.php
+++ post_scripts/getGraphData.php
@@ -2,23 +2,22 @@
 
 include "../db.php";
 
-// Фильтрация ввода данных
-$name = pg_escape_string($_POST['selectedValue']);
-$start = pg_escape_string($_POST['startDate']);
-$end = pg_escape_string($_POST['endDate']);
+// Проверка наличия POST параметров
+if(isset($_POST['selectedValue'], $_POST['startDate'], $_POST['endDate'])) {
+    // Фильтрация ввода данных
+    $name = pg_escape_string($_POST['selectedValue']);
+    $start = pg_escape_string($_POST['startDate']);
+    $end = pg_escape_string($_POST['endDate']);
+    
+    $query = "SELECT valdate, value FROM valute WHERE name = $1 AND valdate BETWEEN $2 AND $3 ORDER BY valdate";
+    $result = pg_query_params($dbconn, $query, array($name, $start, $end));
 
-// Проверка на наличие данных во входящем запросе
-if (empty($name) || empty($start) || empty($end)) {
-    die("Неверные входные данные");
+    if ($result) {
+        $data = pg_fetch_all($result);
+        echo json_encode($data);
+    }
+} else {
+    die('Отсутствуют необходимые данные');
 }
-
-$query = "SELECT valdate, value FROM valute WHERE name = $1 AND valdate BETWEEN $2 AND $3 ORDER BY valdate";
-$result = pg_query_params($dbconn, $query, array($name, $start, $end));
-
-if ($result) {
-    $data = pg_fetch_all($result);
-	echo json_encode($data);
-}
-
 
 ?>
